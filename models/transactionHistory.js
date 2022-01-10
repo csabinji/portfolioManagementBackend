71249efdb0c67a3bd2f1a0a3ab4e2a7ba2903958
sabin <csabinji@gmail.com>
2022-01-10 08:19:14 +0545

    stockname, total and price removed from transaction history

--- models/transactionHistory.js
+++ models/transactionHistory.js
@@ -3,9 +3,6 @@ const { ObjectId } = mongoose.Schema.Types;
 
 const TransactionHistory = mongoose.model(`TransactionHistory`, {
     stockId: { type: ObjectId, ref: 'UserStock', default: null },
-    stockName: { type: String },
-    total: { type: Number },
-    price: { type: Number },
     transactionDate: { type: Date },
     status: { type: String },
 })
